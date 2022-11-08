97abde281cb079e62e442bc023a0bc3fd39e80db
Faiaz Hossain <faiazhossain10@gmail.com>
2022-11-08 14:22:17 +0600

    added banner in home

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,15 +1,28 @@
 import React from "react";
 import image from "../../../images/smiling.jpg";
+import "./Home.css";
 const Home = () => {
   return (
-    <div className="carousel w-full">
+    <div className=" carousel w-full">
       <div id="slide1" className="carousel-item relative w-full">
-        <img src={image} alt="" className="w-full " />
-        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 top-1/3">
+        <div className="carousel-img">
+          <img src={image} alt="" className="w-full " />
+        </div>
+        <div className="absolute flex flex-col justify-between transform -translate-y-1/2 left-5 top-1/2 lg:top-1/3 font-serif">
           <p className="text-sm md:text-lg lg:text-3xl text-white">
             Time To <br />
             <span className="text-2xl md:text-5xl lg:text-8xl">Smile!</span>
           </p>
+          <p className="w-full md:w-1/2 lg:w-1/3 text-white">
+            Ilma is a well-established dentist in the heart of Bangladesh
+            providing excellent dentistry for the whole family. Our priority is
+            making you feel comfortable and at ease in a safe and welcoming
+            environment.
+          </p>
+
+          <button className="btn btn-outline btn-accent w-full md:w-1/2 lg:w-1/3 lg:mt-10">
+            Our services
+          </button>
         </div>
       </div>
     </div>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
     </>
   );
   return (
-    <div className="navbar bg-pink-200">
+    <div className="navbar bg-pink-700 text-white">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
