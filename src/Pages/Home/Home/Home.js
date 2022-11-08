782066a710ea45a2a6828454a3d116edcb1d5857
Faiaz Hossain <faiazhossain10@gmail.com>
2022-11-08 13:51:57 +0600

    added homepage

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
-
+import image from "../../../images/smiling.jpg";
 const Home = () => {
   return (
-    <div>
-      <h2>Home component</h2>
+    <div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img src={image} alt="" className="w-full " />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 top-1/3">
+          <p className="text-sm md:text-lg lg:text-3xl text-white">
+            Time To <br />
+            <span className="text-2xl md:text-5xl lg:text-8xl">Smile!</span>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
